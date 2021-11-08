fb1a201295bd83b2a297c70ac10502416c57028c
Pablo Sarquiz <pablogsarquiz@gmail.com>
2021-11-08 09:01:46 -0300

    W.I.P

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -133,14 +133,14 @@ if (sessionStorage.baseDatos == undefined) {
 
     baseDatos = []
 
-    baseDatos.push(new Libro(1, "KING", "CARRIE", 1974, "DEBOLSILLO", 4, 999, "./ASSETS/Carrie-1981 (1).png"));
-    baseDatos.push(new Libro(2, "KING", "CUJO", 1976, "DEBOLSILLO", 9, 899, "./ASSETS/Cujo.jpg"));
-    baseDatos.push(new Libro(3, "KING", "SALEM'S LOT", 1988, "DEBOLSILLO", 8, 1100, "./ASSETS/salems lot.jpg"));
-    baseDatos.push(new Libro(4, "KING", "IT", 1986, "DEBOLSILLO", 10, 1899, "./ASSETS/It-First-Edition.jpg"));
-    baseDatos.push(new Libro(5, "KING", "PET SAMATARY", 1999, "SALAMANDRA", 5, 999, "./ASSETS/pet.jpg"));
-    baseDatos.push(new Libro(6, "KING", "THE STAND", 1974, "SALAMANDRA", 3, 1499, "./ASSETS/the stand.jpg"));
-    baseDatos.push(new Libro(7, "KING", "THE SHINNING", 1984, "DEBOLSILLO", 0, 1299, "./ASSETS/elResplandor.jpg"));
-    baseDatos.push(new Libro(8, "KING", "MISERY", 1984, "DEBOLSILLO", 3, 999, "./ASSETS/missery.jpg"));
+    baseDatos.push(new Libro(1, "KING", "CARRIE", 1974, "DEBOLSILLO", 4, 999, "./assets/imgs/Carrie-1981 (1).png"));
+    baseDatos.push(new Libro(2, "KING", "CUJO", 1976, "DEBOLSILLO", 9, 899, "./assets/imgs/Cujo.jpg"));
+    baseDatos.push(new Libro(3, "KING", "SALEM'S LOT", 1988, "DEBOLSILLO", 8, 1100, "./assets/imgs/salems lot.jpg"));
+    baseDatos.push(new Libro(4, "KING", "IT", 1986, "DEBOLSILLO", 10, 1899,"./assets/imgs/It-First-Edition.jpg"));
+    baseDatos.push(new Libro(5, "KING", "PET SAMATARY", 1999, "SALAMANDRA", 5, 999, "./assets/imgs/pet.jpg"));
+    baseDatos.push(new Libro(6, "KING", "THE STAND", 1974, "SALAMANDRA", 3, 1499, "./assets/imgs/the stand.jpg"));
+    baseDatos.push(new Libro(7, "KING", "THE SHINNING", 1984, "DEBOLSILLO", 0, 1299,"./assets/imgs/elResplandor.jpg"));
+    baseDatos.push(new Libro(8, "KING", "MISERY", 1984, "DEBOLSILLO", 3, 999,"./assets/imgs/missery.jpg"));
 
 } else {
     let sessionStorageBaseDatos = JSON.parse(sessionStorage.baseDatos);
